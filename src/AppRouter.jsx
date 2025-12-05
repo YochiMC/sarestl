85db43f35f87648af32dc30833e733786f6ff293
Joseph Martinez <josephmartinezcortes5@gmail.com>
2025-12-05 16:07:50 -0600

    last-change

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Welcome } from './pages/Welcome';
 import { Visitas } from "./pages/Visitas";
 import { Registro } from './pages/Registro';
+import { Accesos } from './pages/Accesos';
 import { Layout } from "./pages/layouts/Layout";
 
 export function AppRouter() {
@@ -8,7 +10,9 @@ export function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout children={<Visitas />} />} />
+        <Route path="/dashboard" element={<Layout children={<Welcome />} />} />
         <Route path="/registro" element={<Layout children={<Registro />} />} />
+        <Route path="/accesos" element={<Layout children={<Accesos />} />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Accesos.jsx
@@ -0,0 +1,11 @@
+export function Accesos() {
+    return (
+        <>
+            <h1>Accesos</h1>
+            <div>
+                <h2>Accesos manual</h2>
+                <h2>Acceso con credencial</h2>
+            </div>
+        </>
+    )
+}
