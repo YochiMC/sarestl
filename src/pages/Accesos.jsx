499f254967c379fa67ce7b9589487e450e6f7477
Joseph Martinez <josephmartinezcortes5@gmail.com>
2025-12-06 01:06:16 -0600

    last-change

--- src/pages/Accesos.jsx
+++ src/pages/Accesos.jsx
@@ -4,7 +4,13 @@ export function Accesos() {
             <h1>Accesos</h1>
             <div>
                 <h2>Accesos manual</h2>
+                <label>
+                    Ingresa tu número de control
+                </label>
+                <input name="numeroControl" type="text" placeholder="Ingresa tu número de control">
+                </input>
                 <h2>Acceso con credencial</h2>
+                <p>Pon tu credencial cerca de la cámara</p>
             </div>
         </>
     )
